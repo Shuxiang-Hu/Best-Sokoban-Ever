77682a043893558890c5d942dedf3b4b0ac85408
Shuxiang Hu <shsyh1@nottingham.ac.uk>
2020-11-18 19:21:27 +0000

    Doc GameLogger class
    Java docs are now added to GameLogger Class

--- src/sample/GameLogger.java
+++ src/sample/GameLogger.java
@@ -15,9 +15,16 @@ public class GameLogger extends Logger {
     private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     private Calendar calendar = Calendar.getInstance();
 
+    /**
+     * This constructor creates a directory and a .log file,
+     * adds the handler to that file to the logger, and
+     * sets the format.
+     * @throws IOException on access issues
+     */
     public GameLogger() throws IOException {
         super("GameLogger", null);
 
+        //create a directory
         File directory = new File(System.getProperty("user.dir") + "/" + "logs");
         directory.mkdirs();
 
@@ -27,18 +34,35 @@ public class GameLogger extends Logger {
         fh.setFormatter(formatter);
     }
 
+    /**
+     * This method converts a String into given format
+     * @param message  is the String to be formatted
+     * @return a String in the format "dd/MM/yyyy HH:mm:ss -- message"
+     */
     private String createFormattedMessage(String message) {
         return dateFormat.format(calendar.getTime()) + " -- " + message;
     }
 
+    /**
+     * This method logs INFO in the format "dd/MM/yyyy HH:mm:ss -- INFO"
+     * @param message  is the message to be logged
+     */
     public void info(String message) {
         logger.info(createFormattedMessage(message));
     }
 
+    /**
+     * This method logs warning message in the format "dd/MM/yyyy HH:mm:ss -- WARNING"
+     * @param message  is the message to be logged
+     */
     public void warning(String message) {
         logger.warning(createFormattedMessage(message));
     }
 
+    /**
+     * This method logs severe message in the format "dd/MM/yyyy HH:mm:ss -- SEVERE"
+     * @param message  is the message to be logged
+     */
     public void severe(String message) {
         logger.severe(createFormattedMessage(message));
     }
