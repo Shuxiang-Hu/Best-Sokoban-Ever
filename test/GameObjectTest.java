04fe21fa531b2d212e60e3e3c52c017e09f17413
Shuxiang Hu <shsyh1@nottingham.ac.uk>
2020-11-19 16:01:11 +0000

    Test new constructor for GameObject class
    Test for fromChar method in GameObject class
    
    Test for fromChar is added and passed.

---
+++ test/GameObjectTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import sample.GameObject;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+
+@RunWith(Parameterized.class)
+public class GameObjectTest {
+    private char input;
+    private GameObject expectedOutput;
+    public GameObjectTest(char inputChar, GameObject expectedOutput){
+        this.input=inputChar;
+        this.expectedOutput = expectedOutput;
+    }
+
+    @Parameterized.Parameters
+    public static Collection setParameters(){
+        GameObject []enumerations = GameObject.values();
+        return Arrays.asList(new Object[][]{
+                {'W',enumerations[0]},
+                {' ',enumerations[1]},
+                {'C',enumerations[2]},
+                {'D',enumerations[3]},
+                {'S',enumerations[4]},
+                {'O',enumerations[5]},
+                {'=',enumerations[6]},
+                {'A',enumerations[0]},
+        });
+    }
+
+    @Test
+    public void testFromChar(){
+        System.out.println("Input char is: " + input);
+        GameObject testObject = GameObject.fromChar(input);
+        assertSame(expectedOutput,testObject);
+    }
+}
