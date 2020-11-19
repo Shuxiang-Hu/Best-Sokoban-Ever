e54d0ef5fa438d078475b7c6f2553df216bdb21f
Shuxiang Hu <shsyh1@nottingham.ac.uk>
2020-11-19 13:06:51 +0000

    Update GameObject class
    Getters are moved to the top of all methods. Field 'symbol' renamed into SYMBOL.

--- src/sample/GameObject.java
+++ src/sample/GameObject.java
@@ -9,10 +9,26 @@ public enum GameObject {
     CRATE_ON_DIAMOND('O'),
     DEBUG_OBJECT('=');
 
-    private final char symbol;
+    private final char SYMBOL;
 
-    GameObject(final char symbol) {
-        this.symbol = symbol;
+    GameObject(final char SYMBOL) {
+        this.SYMBOL = SYMBOL;
+    }
+
+    /**
+     * This method is used to get the symbol of a GameObject object
+     * @return GameObject returns a string object version of the symbol
+     */
+    public String getStringSymbol() {
+        return String.valueOf(SYMBOL);
+    }
+
+    /**
+     * This method is used to get the symbol of a GameObject object
+     * @return GameObject returns the symbol of a GameObject object
+     */
+    public char getCharSymbol() {
+        return SYMBOL;
     }
 
     /**
@@ -24,7 +40,7 @@ public enum GameObject {
      */
     public static GameObject fromChar(char c) {
         for (GameObject t : GameObject.values()) {
-            if (Character.toUpperCase(c) == t.symbol) {
+            if (Character.toUpperCase(c) == t.SYMBOL) {
                 return t;
             }
         }
@@ -32,19 +48,5 @@ public enum GameObject {
         return WALL;
     }
 
-    /**
-     * This method is used to get the symbol of a GameObject object
-     * @return GameObject returns a string object version of the symbol
-     */
-    public String getStringSymbol() {
-        return String.valueOf(symbol);
-    }
 
-    /**
-     * This method is used to get the symbol of a GameObject object
-     * @return GameObject returns the symbol of a GameObject object
-     */
-    public char getCharSymbol() {
-        return symbol;
-    }
 }
