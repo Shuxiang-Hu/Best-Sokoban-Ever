a0ca88cbcfec70f5ab44ab3fc8fe43446d87b832
Shuxiang HU <shysh1@exmail.nottingham.ac.uk>
2020-12-03 19:33:48 +0000

    Expand Undo
    Now the player can undo as many times as he/she wishes, but this will
    cost a move.

--- src/object/GameCrateOnDiamond.java
+++ src/object/GameCrateOnDiamond.java
@@ -1,5 +1,9 @@
 package object;
-
+/**
+ * Represents a crate on a diamond, which is subclass of GameObject
+ * with a symbol character 'O'
+ * @author Shuxiang Hu
+ */
 public class GameCrateOnDiamond extends GameObject {
     public GameCrateOnDiamond() {
         setCharSymbol('O');
--- src/object/GameDebugObject.java
+++ src/object/GameDebugObject.java
@@ -1,5 +1,9 @@
 package object;
-
+/**
+ * Represents a debug object, which is subclass of GameObject
+ * with a symbol character '='
+ * @author Shuxiang Hu
+ */
 public class GameDebugObject extends GameObject {
     public GameDebugObject() {
         setCharSymbol('=');
